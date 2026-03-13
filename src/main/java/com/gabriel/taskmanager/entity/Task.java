2600d0b742ca65869ddf5c716d19608d14215083
Gabriel Alvews <lgabrielalvesgm@gmail.com>
2026-03-12 22:02:24 -0300

    Definição da arquitetura do projeto + criação das entidades Principais

---
+++ src/main/java/com/gabriel/taskmanager/entity/Task.java
@@ -0,0 +1,47 @@
+package com.gabriel.taskmanager.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="tasks")
+@Data
+@NoArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String titulo;
+
+    private String descricao;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status = TaskStatus.OPEN;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
+
+enum TaskStatus {
+    OPEN,
+    IN_PROGRESS,
+    DONE
+}
---
+++ src/main/java/com/gabriel/taskmanager/entity/User.java
@@ -0,0 +1,44 @@
+package com.gabriel.taskmanager.entity;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    private String descricao;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @OneToMany(mappedBy="users", cascade=CascadeType.ALL)
+    private List<Task> tasks = new ArrayList<>();
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+}
