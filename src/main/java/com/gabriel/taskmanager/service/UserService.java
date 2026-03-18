5a63567dcfb8efe13ed88643e072eda4773ecc53
Gabriel Alvews <lgabrielalvesgm@gmail.com>
2026-03-18 20:38:15 -0300

    Ajuste e criação dos métodos de buscar usuário.

--- src/main/java/com/gabriel/taskmanager/service/UserService.java
+++ src/main/java/com/gabriel/taskmanager/service/UserService.java
@@ -1,6 +1,5 @@
 package com.gabriel.taskmanager.service;
 
-
 import com.gabriel.taskmanager.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import com.gabriel.taskmanager.dto.UserDTO;
@@ -10,23 +9,36 @@ import jakarta.validation.Valid;
 @Service
 public class UserService {
 
-    //Repositório genérico para referência
+    // Repositório genérico para referência
     private final UserRepository userRepository;
-    
+
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    public UserDTO getUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setNome(user.getNome());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setCreatedAt(user.getCreatedAt());
+
+        return userDTO;
+    }
+
     public UserDTO createUser(@Valid UserDTO userDTO) {
 
-        //DTO -> ENTIDADE
+        // DTO -> ENTIDADE
         User user = new User();
         user.setNome(userDTO.getNome());
         user.setEmail(userDTO.getEmail());
-        
+
         User savedUser = userRepository.save(user);
 
-        //ENTIDADE -> DTO
+        // ENTIDADE -> DTO
         UserDTO responseDTO = new UserDTO();
         responseDTO.setId(savedUser.getId());
         responseDTO.setNome(savedUser.getNome());
